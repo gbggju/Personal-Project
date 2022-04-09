b3242f4e1902930334ca9e79c87e20fbc06c9044
gbggju <davedareal1@gmail.com>
2022-04-09 08:52:42 -0700

    added learn.py

---
+++ learn.py
@@ -0,0 +1,50 @@
+
+# i = 0
+# while i >= 2:
+#     print ('fall')
+# else: 
+    
+#     i += 1
+
+#     print (i / 5)
+    
+# height = 
+# if int(height) <= 10:
+#     print ('youre too short')
+# elif int(height) >= 10:
+#     print ('youre too tall')
+# elif int(height) == 10:
+#     print ('you can ride ;)')
+# else:
+#     print('go home')
+
+# for x in range(0, 10, 2):
+#     print (x)
+
+# passcode = input('insert passcode: ')
+
+
+# if passcode == 1234:
+#     print ('well done')
+# else:
+#     print ('try again ' + input)
+# pass
+
+
+
+players = ("gundo", 'kdb', 'zinny', 'sterling')
+text = 'get a better job david and stop being an embarrasment to your parents'
+
+
+
+text[0:0] = 'david'
+
+
+print (text)
+
+# players2 = ('ferna', 'cancelo')
+
+# combined = (players2) + (players)
+
+# print (combined)
+
